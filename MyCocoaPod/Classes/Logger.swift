ab4f467c123d6323d800d71e73ea93989bd546fd
Pradeep N <pradeepn@nousinfo.com>
2020-11-13 17:25:18 +0530

    first commit

---
+++ MyCocoaPod/Classes/Logger.swift
@@ -0,0 +1,21 @@
+//
+//  Logger.swift
+//  MyCocoaPod
+//
+//  Created by Pradeep on 13/11/20.
+//
+
+import Foundation
+
+public class Logger {
+    
+    public init() {}
+    
+    public func printLog(){
+        print("Logger")
+    }
+    
+    func privateMethod(){
+        print("Private method")
+    }
+}
